22b01d1b7c93269fd177090c96d0c37d89d51b93
Ben Larabie <benlarabie@gmail.com>
2019-03-11 11:42:45 -0400

    script for pushing

---
+++ pushRepo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export AZ_SP=$1
+export AZ_SP_PASS=$2
+export AZ_TENANT=$3
+export AZ_SHARE_KEY=$4
+export WORKSPACE=$5
+az login --service-principal --username $AZ_SP --password $AZ_SP_PASS --tenant $AZ_TENANT
+az storage file upload-batch --source $WORKSPACE/helm-charts --destination / --account-name d38dedd245ba4c1fb2026acd --account-key $AZ_SHARE_KEY
+
+
