85b671aa4eaa1952a22d2218ce68d057b63683fb
Ben Larabie <benlarabie@gmail.com>
2019-03-11 10:40:37 -0400

    Helm initialization on build script

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 export workspace=$1
-mkdir $workspace/helm-charts
+mkdir -p $workspace/helm-charts
+
+#Check if helm installed
+if ! helm init --client-only;
+then
+    wget https://storage.googleapis.com/kubernetes-helm/helm-v2.13.0-linux-amd64.tar.gz
+    tar -zxvf helm-v2.13.0-linux-amd64.tar.gz
+    mv linux-amd64/helm /usr/local/bin/helm
+    if ! helm init --client-only;
+    then
+        echo "Unable to initalize Helm"
+        exit 1
+    fi
+fi
+
 find . -type f -iname "Chart.yaml" -print0 | while IFS= read -r -d $'\0' line; do
     export realPath=`realpath $line`
     export directory=`dirname $realPath`
