37f50326464ea896ad5c26d1c2e3bfacf9531d6d
Ben Larabie <benlarabie@gmail.com>
2019-03-11 12:08:50 -0400

    Working push - removing debugging

--- pushRepo.sh
+++ pushRepo.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-export AZ_SP=$1
-export AZ_SP_PASS=$2
-export AZ_TENANT=$3
-export AZ_SHARE_KEY=$4
-export WORKSPACE=$5
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-
 az login --service-principal --username $1 --password $2 --tenant $3
-az storage file upload-batch --source $WORKSPACE/helm-charts --destination / --account-name d38dedd245ba4c1fb2026acd --account-key $4 --dryrun
-
+az storage file upload-batch --source $WORKSPACE/helm-charts --destination / --account-name d38dedd245ba4c1fb2026acd --account-key $4
 
