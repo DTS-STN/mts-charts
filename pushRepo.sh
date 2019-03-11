fd693e1662fb5190d1fb94bf480360e63568e494
Ben Larabie <benlarabie@gmail.com>
2019-03-11 12:00:29 -0400

    debugging push script

--- pushRepo.sh
+++ pushRepo.sh
@@ -4,13 +4,13 @@ export AZ_SP_PASS=$2
 export AZ_TENANT=$3
 export AZ_SHARE_KEY=$4
 export WORKSPACE=$5
-echo $AZ_SP
-echo $AZ_SP_PASS
-echo $AZ_TENANT
-echo $AZ_SHARE_KEY
-echo $WORKSPACE
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
 
-az login --service-principal --username $AZ_SP --password $AZ_SP_PASS --tenant $AZ_TENANT
-az storage file upload-batch --source $WORKSPACE/helm-charts --destination / --account-name d38dedd245ba4c1fb2026acd --account-key $AZ_SHARE_KEY --dryrun
+az login --service-principal --username $1 --password $2 --tenant $3
+az storage file upload-batch --source $WORKSPACE/helm-charts --destination / --account-name d38dedd245ba4c1fb2026acd --account-key $4 --dryrun
 
 
