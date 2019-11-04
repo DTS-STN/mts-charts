89e90e47b2efda53f63555f56624641939a41d9b
Ben Larabie <benlarabie@gmail.com>
2019-11-04 15:10:29 -0500

    reverting push repo script

--- pushRepo.sh
+++ pushRepo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 az login --service-principal --username $1 --password $2 --tenant $3
-az storage file upload-batch --source $WORKSPACE/built-charts --destination mtsfileshare --destination-path /dts-charts --account-name $TF_VAR_STORAGE_ACCT_NAME --account-key $4
+az storage file upload-batch --source $WORKSPACE/helm-charts --destination mtsfileshare --destination-path /dts-charts --account-name d38dedd245ba4c1fb2026acd --account-key $4
 
